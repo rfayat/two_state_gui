d2eb01b222a63ae3205833b1171a5606c2dd35ff
rfayat <r.fayat@gmail.com>
2021-05-23 17:27:43 +0200

    Add click event on the fit curve

--- GUI_outlier_correction/two_state_gui.py
+++ GUI_outlier_correction/two_state_gui.py
@@ -10,6 +10,7 @@ import pandas as pd
 from .simulation import Data_Simulator
 from .data_handling import HMM_State_Handler
 import numpy as np
+import json
 
 colors = {"navbar": "#17A2B8", "plotly": "#119DFF"}
 
@@ -25,14 +26,21 @@ handler.add_fitted_states(simulator.states)
 
 # Generate the figure
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=handler.time,
-                         y=simulator.data,
-                         mode='lines', name='Data', hoverinfo="skip"))
+trace_data = go.Scatter(x=handler.time,
+                        y=simulator.data,
+                        mode="lines", name="Data", hoverinfo="skip")
 
-fig.add_trace(go.Scatter(x=handler.get_intervals_time().flatten(),
-                         y=handler.get_mu(handler.intervals_states.repeat(2)),
-                         mode='lines', name='Fit'))
-
+trace_fit = go.Scatter(x=handler.get_intervals_time().flatten(),
+                       y=handler.get_mu(handler.intervals_states.repeat(2)),
+                       mode="lines", name="Fit", hoverinfo="skip")
+trace_fit_corrected = go.Scatter(
+    x=handler.get_intervals_time().flatten(),
+    y=handler.get_mu(handler.intervals_states_corrected.repeat(2)),
+    mode="lines", name="Corrected"
+)
+fig.add_trace(trace_data)
+fig.add_trace(trace_fit)
+fig.add_trace(trace_fit_corrected)
 fig.update_xaxes(rangeslider_visible=True)
 
 # Create the app and the layout
@@ -87,20 +95,25 @@ button_group_action = dbc.Row([
     ),
 ], justify="center")
 
+# Buttons for I/O and HMM fit
+button_group_hmm = dbc.ButtonGroup([
+    dbc.Button("Upload CSV", color="info", className="mb-3"),
+    dbc.Button("Export to CSV", color="info", className="mb-3"),
+    dbc.Button("Import fit", color="info", className="mb-3"),
+    dbc.Button("Compute fit", id="collapse-button",
+               className="mb-3", color="info"),
+], className="mb-3")
+
 # Global layout of the app
 app.layout = html.Div([
     navbar,
-    dcc.Graph(id='data_graph', figure=fig),
+    dcc.Graph(id="data_graph", figure=fig),
     button_group_action,
     html.Div([
-        dbc.ButtonGroup([
-            dbc.Button("Upload CSV", color="info", className="mb-3"),
-            dbc.Button("Export to CSV", color="info", className="mb-3"),
-            dbc.Button("Edit fit parameters", id="collapse-button",
-                       className="mb-3", color="info"),
-        ], className="mb-3"),
+        button_group_hmm,
         collapse_hmm
     ]),
+    html.Div([], id="out")
 ])
 
 # Interactive elements
@@ -108,10 +121,19 @@ app.layout = html.Div([
               [Input("collapse-button", "n_clicks")],
               [State("collapse", "is_open")])
 def toggle_collapse(n, is_open):
+    "Collapse the HMM fit section"
     if n:
         return not is_open
     return is_open
 
+@app.callback(Output("out", "children"),
+              Input("data_graph", "clickData"))
+def display_click_data(clickData):
+    out = json.dumps(clickData, indent=2)
+    print(out)
+    return out
 
-if __name__ == '__main__':
-    app.run_server(host='127.0.0.1', debug=True)
+
+
+if __name__ == "__main__":
+    app.run_server(host="127.0.0.1", debug=True)
