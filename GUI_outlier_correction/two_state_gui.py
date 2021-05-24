6fd5652dd0c30225b368298289a0be8277365fd6
rfayat <r.fayat@gmail.com>
2021-05-24 22:54:02 +0200

    Add data generation button

--- GUI_outlier_correction/two_state_gui.py
+++ GUI_outlier_correction/two_state_gui.py
@@ -15,24 +15,27 @@ from .data_handling import HMM_State_Handler
 from functools import wraps
 
 
-colors = {"navbar": "#17A2B8",
+COLORS = {"navbar": "#17A2B8",
           "plotly": "#119DFF",
           "data": "#636EFA",
           "fit": "#F0644D",
           "corrected": "#27D3A6",
           "ignored": "Black"}
 
-# Simulate data
-N_POINTS = 100000
-simulator = Data_Simulator.simulate(n_points=N_POINTS, mu_all=[.2, .4])
-# Simulate missing states
-simulated_states = simulator.states
-simulated_states[int(N_POINTS / 2):int(N_POINTS / 2) + int(N_POINTS / 20)] = -1
-# Create the handler for the states
-handler = HMM_State_Handler.from_parameters(
-    mu_all=simulator.mu_all, sigma_all=simulator.sigma_all
-)
-handler.add_fitted_states(simulated_states)
+
+def update_traces(fig, handler, data):
+    "Update the traces with the raw and corrected data."
+    # Trace the raw data
+    fig.update_traces(x=handler.time, y=data,
+                      selector={"name": "Data"})
+    # Trace a compressed version of the initial fit
+    idx_to_plot = np.c_[handler.intervals_start,
+                        handler.intervals_end - 1].flatten()
+    fig.update_traces(x=handler.time[idx_to_plot],
+                      y=handler.get_mu(handler.states[idx_to_plot]),
+                      selector={"name": "Fit"})
+    fig = update_corrected_traces(fig, handler)  # Plot the corrected state
+    return fig
 
 
 def update_corrected_traces(fig, handler):
@@ -77,43 +80,28 @@ def update_corrected_traces(fig, handler):
 
 # Generate the figure
 fig = go.Figure()
-
 # Add the data trace using scattergl (much faster than scatter)
-trace_data = go.Scattergl(x=handler.time,
-                          y=simulator.data,
-                          line={"color": colors["data"]},
+trace_data = go.Scattergl(line={"color": COLORS["data"]},
                           mode="lines",
                           name="Data",
                           hoverinfo="skip")
 
-intervals_number = np.repeat(np.arange(handler.n_intervals),
-                             handler.intervals_durations)
-trace_fit_corrected = go.Scattergl(
-    line={"color": colors["corrected"], "width": 3.},
-    mode="lines", name="Corrected"
-)
-trace_ignored = go.Scattergl(
-    marker_symbol="line-ew",
-    marker_line_width=4.,
-    marker_size=5,
-    line={"color": colors["ignored"]},
-    mode="markers", name="Ignored"
-)
-# Trace a compressed version of the initial fit
-idx_to_plot = np.c_[handler.intervals_start,
-                    handler.intervals_end - 1].flatten()
-trace_fit = go.Scattergl(
-    x=handler.time[idx_to_plot],
-    y=handler.get_mu(handler.states[idx_to_plot]),
-    line={"color": colors["fit"]},
-    mode="lines", name="Fit", hoverinfo="skip"
-)
+trace_fit_corrected = go.Scattergl(line={"color": COLORS["corrected"],
+                                         "width": 3.},
+                                   mode="lines", name="Corrected")
+trace_ignored = go.Scattergl(marker_symbol="line-ew",
+                             marker_line_width=4.,
+                             marker_size=5,
+                             line={"color": COLORS["ignored"]},
+                             mode="markers", name="Ignored")
+trace_fit = go.Scattergl(line={"color": COLORS["fit"]},
+                         mode="lines", name="Fit", hoverinfo="skip")
 fig.add_trace(trace_data)
 fig.add_trace(trace_fit)
 fig.add_trace(trace_fit_corrected)
 fig.add_trace(trace_ignored)
 fig.update_xaxes(rangeslider_visible=True)
-fig = update_corrected_traces(fig, handler)  # Plot the corrected state
+
 
 # Create the app and the layout
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -121,30 +109,33 @@ app.title = "Two-State GUI"
 
 # Collapse for setting the HMM parameters
 collapse_hmm = dbc.Collapse(
-    dbc.Card(dbc.CardBody(
-        html.Div([
-            dbc.Row([dbc.Button("Fit HMM",
-                                color="success",
-                                className="mb-1")]),
-            dbc.Row([
-                dbc.InputGroup([
-                    dbc.InputGroupAddon("param1",
-                                        addon_type="prepend"),
-                    dbc.Input(placeholder="param1",
-                              type="number")],
-                              className="mb-1",
-                ),
-                dbc.InputGroup([
-                    dbc.InputGroupAddon("param2",
-                                        addon_type="prepend"),
-                    dbc.Input(placeholder="param2",
-                              type="number")],
-                    className="mb-1",
-                ),
+    dbc.Card(
+        dbc.CardBody(
+            html.Div([
+                dbc.Row([dbc.Button("Fit HMM",
+                                    color="success",
+                                    className="mb-1")]),
+                dbc.Row([
+                    dbc.InputGroup([
+                        dbc.InputGroupAddon("param1",
+                                            addon_type="prepend"),
+                        dbc.Input(placeholder="param1",
+                                  type="number")],
+                                  className="mb-1",
+                    ),
+                    dbc.InputGroup([
+                        dbc.InputGroupAddon("param2",
+                                            addon_type="prepend"),
+                        dbc.Input(placeholder="param2",
+                                  type="number")],
+                        className="mb-1",
+                    ),
+                ]),
             ]),
-        ]),
-    )),
-    id="collapse"
+        ),
+        className="border-0"
+    ),
+    id="collapse",
 )
 
 # Top navigation bar
@@ -153,58 +144,68 @@ navbar = dbc.Navbar([
             [dbc.NavbarBrand("Two-State GUI")]
         ),
     ],
-    color=colors["navbar"],
+    color=COLORS["navbar"],
     dark=True,
 )
 
 # Radio buttons
-radio_action = dcc.RadioItems(
-    options=[
-        {"label": "Toggle", "value": "toggle"},
-        {"label": "Discard", "value": "discard"},
-    ],
-    id="radio_action",
-    value="toggle",
-    labelStyle={"display": "inline-block"}
-)
+radio_action = dbc.Card([
+    dbc.CardBody([
+        html.H5("Action on click", className="card-title"),
+        dbc.RadioItems(
+            options=[
+                {"label": "Toggle State", "value": "toggle"},
+                {"label": "Ignore Values", "value": "discard"},
+            ],
+            id="radio_action",
+            value="toggle",
+        )
+    ]),
+], style={"width": "18rem"})
 
 # Buttons for I/O and HMM fit
 button_group_hmm = dbc.ButtonGroup([
+    dbc.Button("Simulate data", id="simulate-button",
+               color="info", className="mb-3"),
     dbc.Button("Upload CSV", color="info", className="mb-3"),
     dbc.Button("Export to CSV", color="info", className="mb-3"),
     dbc.Button("Import fit", color="info", className="mb-3"),
     dbc.Button("Compute fit", id="collapse-button",
                className="mb-3", color="info"),
-], className="mb-3")
+], className="mt-6 ml-3")
 
 # Global layout of the app
 app.layout = html.Div([
     navbar,
     dcc.Graph(id="data_graph", figure=fig),
-    radio_action,
-    html.Div([
-        button_group_hmm,
-        collapse_hmm
-    ]),
-    html.Div([], id="out")
+    dbc.Row([
+        dbc.Col(radio_action, width={"offset": 1}),
+        dbc.Col(
+            html.Div([
+                button_group_hmm,
+                collapse_hmm
+            ]),
+            width={"size": 4, "align": "end"}
+        ),
+    ], justify="between")
 ])
 
 
-# Interactive elements
-def preserve_xrange(f):
-    "Make sure the x axis' range is maintained after calling the function."
-    @wraps(f)
-    def g(*args, **kwargs):
-        global fig
-        # TODO: not working, even when using fig.full_figure_for_development
-        # Grab the x axis' range before running the function
-        xrange_before = fig.layout.xaxis.range  # noqa W0612
-        # Run the function
-        out = f(*args, **kwargs)
-        # Set the x axis' range to its initial value
-        # TODO
-        return out
-    return g
+# # Interactive elements
+# def preserve_xrange(f):
+#     "Make sure the x axis' range is maintained after calling the function."
+#     @wraps(f)
+#     def g(*args, **kwargs):
+#         global fig
+#         # TODO: not working, even when using fig.full_figure_for_development
+#         # Grab the x axis' range before running the function
+#         xrange_before = fig.layout.xaxis.range  # noqa W0612
+#         # Run the function
+#         out = f(*args, **kwargs)
+#         # Set the x axis' range to its initial value
+#         # TODO
+#         return out
+#     return g
 
 
 @app.callback(Output("collapse", "is_open"),
@@ -217,33 +218,62 @@ def toggle_collapse(n, is_open):
     return is_open
 
 
+def run_simulation(fig):
+    "Generate data and update the graph."
+    global handler
+
+    # Simulate data
+    N_POINTS = 100000
+    simulator = Data_Simulator.simulate(n_points=N_POINTS, mu_all=[.2, .4])
+    # Simulate missing states
+    simulated_states = simulator.states
+    simulated_states[int(N_POINTS / 2):int(N_POINTS / 2) + int(N_POINTS / 20)] = -1  # noqa E501
+    # Create the handler for the states
+    handler = HMM_State_Handler.from_parameters(
+        mu_all=simulator.mu_all, sigma_all=simulator.sigma_all
+    )
+    handler.add_fitted_states(simulated_states)
+    fig = update_traces(fig, handler, simulator.data)
+    fig['layout']['xaxis'].update(range=[handler.time[0], handler.time[-1]])
+    return fig
+
+
 @app.callback(Output("data_graph", "figure"),
               Input("data_graph", "clickData"),
+              Input("simulate-button", "n_clicks"),
               State("radio_action", "value"))
-@preserve_xrange
-def change_interval_state(clickData, action):
-    "Change the state of an interval."
-    global handler
+def change_interval_state(clickData, simulate_click, action):
+    "Handle callbacks affecting the output figure."
     global fig
-    # Ignore clicks not on a point
-    if clickData is None:
+    global handler
+    # WARNING: Dash doesn't apparently support multiple callbacks with figure
+    # as output hence this very circumvolute implementation...
+    # Ids object whose status has been changed
+    changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
+    # Select the action to perform based on the inputs and changed_id
+    trigger_change_interval = clickData is not None and action is not None and changed_id == "data_graph.clickData"  # noqa E501
+    trigger_simulation = changed_id == "simulate-button.n_clicks" and simulate_click is not None  # noqa 501
+
+    if trigger_change_interval:
+        interval_idx = int(clickData["points"][0]["customdata"][0])
+        # Toggle the state of the interval or discard depending on the inputs
+        if action == "toggle":
+            handler.change_interval_state(interval_idx)
+        else:
+            handler.change_interval_missing_status(interval_idx)
+
+        # Update the traces and return the updated figure
+        fig = update_corrected_traces(fig, handler)
+        # Redraw using a time interval around the change
+        interval_start_time = clickData["points"][0]["customdata"][3]
+        interval_end_time = clickData["points"][0]["customdata"][4]
+        time_range = [interval_start_time - 60, interval_end_time + 60]
+        fig['layout']['xaxis'].update(range=time_range)
         return fig
-
-    interval_idx = clickData["points"][0]["customdata"][0]
-    # Toggle the state of the interval or discard depending on the radio inputs
-    if action == "toggle":
-        handler.change_interval_state(interval_idx)
+    elif trigger_simulation:  # noqa E501
+        return run_simulation(fig)
     else:
-        handler.change_interval_missing_status(interval_idx)
-
-    # Update the traces and return the updated figure
-    fig = update_corrected_traces(fig, handler)
-    # Redraw using a time interval around the change
-    interval_start_time = clickData["points"][0]["customdata"][3]
-    interval_end_time = clickData["points"][0]["customdata"][4]
-    time_range = [interval_start_time - 60, interval_end_time + 60]
-    fig['layout']['xaxis'].update(range=time_range)
-    return fig
+        return fig
 
 
 if __name__ == "__main__":
